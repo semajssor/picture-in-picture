b129751aea0950bb52f7302ba0572fdb819a194b
semajssor <141041402+semajssor@users.noreply.github.com>
2024-03-27 10:20:36 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,32 @@
+const videoElement = document.getElementById('video');
+const button = document.getElementById('button');
+
+// Prompt to select media stream, pass to video element, then play
+async function selectMediaStream() {
+   try {
+      const mediaStream = await navigator.mediaDevices.getDisplayMedia();
+      videoElement.srcObject = mediaStream;
+      videoElement.onloadedmetadata = () => {
+         videoElement.play();
+      };
+   } catch (error) {
+      console.log(error);
+   }
+}
+
+button.addEventListener('click', async () => {
+   // Disable button
+   button.disabled = true;
+
+   // Start PiP
+   await videoElement.requestPictureInPicture();
+
+   // Reset button
+   button.disabled = false;
+});
+
+
+
+
+// On load
+selectMediaStream();
